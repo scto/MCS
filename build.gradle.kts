7d9a4a1fcfeefcb8d628dc3039f00b0eaef520bf
scto <tschmid35@gmail.com>
2026-04-11 16:38:30 +0000

    Revert "build: configure core/common module and add kotlin-kapt plugin"
    This reverts commit e256b80cd773b17535d05e98d0ae5f3c9ab9a40a.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.hilt.android) apply false
 }
 
-android {
-    namespace = "com.scto.mcs.core.common"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 26
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.hilt.android.gradle.plugin)
     }
 }
