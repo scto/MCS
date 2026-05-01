21235e103d29d266baf151aa734c796651bd9c4a
scto <tschmid35@gmail.com>
2026-05-01 19:54:39 +0200

    fix: app/assets

--- feature/onboarding/src/main/java/com/scto/mcs/feature/onboarding/OnboardingState.kt
+++
@@ -1,7 +0,0 @@
-package com.scto.mcs.feature.onboarding
-
-data class OnboardingState(
-    val currentPage: Int = 0,
-    val isPermissionGranted: Boolean = false,
-    val isCompleted: Boolean = false
-)
